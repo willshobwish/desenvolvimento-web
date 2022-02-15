b0cac1f44bb9a5708f49d2620c5fc33439b1000b
willshobwish <willianyos13@gmail.com>
2022-02-15 09:57:47 -0300

    Criar o aplicativo mínimo em Flask

---
+++ Exercicios/Aula-21/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Credenciais inválidos, tente novamente.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
